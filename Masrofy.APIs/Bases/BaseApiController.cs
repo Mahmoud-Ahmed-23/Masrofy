b004780b2f9c67afe8e95d7d7f72821d19b9b931
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2025-04-26 13:06:41 +0300

    Add project files.

---
+++ Masrofy.APIs/Bases/BaseApiController.cs
@@ -0,0 +1,37 @@
+﻿using Masrofy.Application.Bases;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Masrofy.APIs.Bases
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BaseApiController : ControllerBase
+	{
+		private IMediator mediator;
+		protected IMediator Mediator => mediator ??= HttpContext.RequestServices.GetService<IMediator>()!;
+		
+		public ObjectResult NewResult<T>(Response<T> response)
+		{
+			switch (response.StatusCode)
+			{
+				case HttpStatusCode.OK:
+					return new OkObjectResult(response);
+				case HttpStatusCode.Created:
+					return new CreatedResult(string.Empty, response);
+				case HttpStatusCode.Unauthorized:
+					return new UnauthorizedObjectResult(response);
+				case HttpStatusCode.BadRequest:
+					return new BadRequestObjectResult(response);
+				case HttpStatusCode.NotFound:
+					return new NotFoundObjectResult(response);
+				case HttpStatusCode.Accepted:
+					return new AcceptedResult(string.Empty, response);
+				default:
+					return new BadRequestObjectResult(response);
+			}
+		}
+	}
+}
